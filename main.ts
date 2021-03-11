84e2cd49c7ff65a4325d0a5675a92923dd6f9b4d
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-11 12:28:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+// when A is pressed, the computer randomly chooses a no# between -20 & 20. if that no# is between 0 & 10 display that no#, otherwise display X (less lines to use)
+input.onButtonPressed(Button.A, function () {
+	
+})
+// when B is pressed, the computer randomly chooses a no# between -20 & 20. if that no# is between 0 & 10 display that no#, otherwise display X (more lines)
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
